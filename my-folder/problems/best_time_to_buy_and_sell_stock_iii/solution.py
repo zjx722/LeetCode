bdf7e5c9d8bd367c505452e323e230685e9ec89f
Jiaxin <zjx722@gmail.com>
2022-04-13 22:24:21 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (python3)

---
+++ my-folder/problems/best_time_to_buy_and_sell_stock_iii/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        t1_cost, t2_cost = float('inf'), float('inf')
+        t1_profit, t2_profit = 0, 0
+
+        for price in prices:
+            # the maximum profit if only one transaction is allowed
+            t1_cost = min(t1_cost, price)
+            t1_profit = max(t1_profit, price - t1_cost)
+            # reinvest the gained profit in the second transaction
+            t2_cost = min(t2_cost, price - t1_profit)
+            t2_profit = max(t2_profit, price - t2_cost)
+
+        return t2_profit
+        
