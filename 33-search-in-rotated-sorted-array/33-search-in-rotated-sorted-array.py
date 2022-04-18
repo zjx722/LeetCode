4901a762951671186969c679778c0e2f19d6ecfe
Jiaxin <zjx722@gmail.com>
2022-04-17 18:13:53 -0700

    Time: 46 ms (75.68%), Space: 14.3 MB (20.39%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        
+        left, right = 0, len(nums)-1
+        
+        while left <= right:
+            mid = left + (right-left)//2
+            if target  ==  nums[mid]:
+                return mid
+            
+            # left portion
+            elif nums[left] <= nums[mid] :
+                if target > nums[mid] or target < nums[left]:
+                    left = mid+1
+                else:
+                    right = mid-1
+                    
+            #right portion        
+            else:
+                if target < nums[mid] or target > nums[right]:
+                    right = mid-1
+                else:
+                    left = mid +1
+                               
+        return -1
+        
