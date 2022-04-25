91bc1f0f1e472f191450499154ab4072e0d7c413
Jiaxin <zjx722@gmail.com>
2022-04-25 02:43:47 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (python3)

--- my-folder/problems/best_time_to_buy_and_sell_stock_ii/solution.py
+++ my-folder/problems/best_time_to_buy_and_sell_stock_ii/solution.py
@@ -15,8 +15,17 @@ class Solution:
             prev_fast = fast            
             fast +=1
             
-
-            
         return res
+    
+#another#    
+# public int maxProfit(int[] prices) {
+#         int maxprofit = 0;
+#         for (int i = 1; i < prices.length; i++) {
+#             if (prices[i] > prices[i - 1])
+#                 maxprofit += prices[i] - prices[i - 1];
+#         }
+#         return maxprofit;
+#     }
+# }
                 
                 
